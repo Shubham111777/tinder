7c430743bf5fbdfb5ce418ee210ea37a7597b1e5
Shubham Jaiswal <Shubham.jaiswal@maiora.co>
2025-11-13 14:45:46 +0530

    route handler

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
-const express = require('express');
-
+const express = require("express");
 const app = express();
 
-app.use("/test",(req,res)=>{
-res.send("Hello World");
-})
-app.listen(8080 , ()=>{
-    console.log("Server is running on port 8080");
-})
+app.use("/user",[ (req, res,next) => {
+  console.log("route111")
+  next();
+  res.send("route1");
+},(req, res)=>{
+console.log("route2222")
+  res.send("route2");
+
+
+}]);
+
+app.listen(8080 , () => {
+  console.log("Server is running on port 8080");
+});
